ba7147e88f2e53f2263914ed51f265ff2747a6d9
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-22 00:41:18 -0300

    refactor: Routes e app.js

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import userRoutes from "./modules/user/routes/UserRoutes.js";
 
 const app = express();
 
 const env = process.env;
 const PORT = env.PORT || 8080;
+
+db.createInitialData();
+
+app.user(express.json());
+
+app.use(userRoutes);
 
 // Rota para verificar o status da API
 app.get("/api/status", (req, res) => {
