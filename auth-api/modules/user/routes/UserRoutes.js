c195c8becf1f2883c435b5534cf1f0090ed646dc
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-22 01:30:56 -0300

    fix: correct route path and add .js extension to imports

--- auth-api/modules/user/routes/UserRoutes.js
+++ auth-api/modules/user/routes/UserRoutes.js
@@ -1,9 +1,8 @@
-import { Router } from "express"
+import { Router } from "express";
+import UserController from "../controller/UserController.js";
 
-import UserController from "../controller/UserController"
+const router = Router(); 
 
-const router = new Router();
-
-router.get("api/user/email/:email", UserController.findByEmail);
+router.get("/api/user/email/:email", UserController.findByEmail);
 
 export default router;
