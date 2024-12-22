9da06a66c545d81961e8f5e0e57cdcb7ab5aa201
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-22 01:25:16 -0300

    refactor: remove database initialization from app startup

--- auth-api/app.js
+++ auth-api/app.js
@@ -6,9 +6,7 @@ const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
-db.createInitialData();
-
-app.user(express.json());
+app.use(express.json());
 
 app.use(userRoutes);
 
