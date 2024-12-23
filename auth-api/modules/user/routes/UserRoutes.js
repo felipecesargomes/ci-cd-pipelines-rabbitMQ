b8457adc3d2e4548c6386a66f7d5b31bf153fb9d
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-22 23:16:52 -0300

    feat: implements UserRoutes with authentication and token validation

--- auth-api/modules/user/routes/UserRoutes.js
+++ auth-api/modules/user/routes/UserRoutes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import UserController from "../controller/UserController.js";
+import CheckToken from "../../../config/auth/CheckToken.js";
 
-const router = Router(); 
+const router = Router();
 
+// Rotas públicas (sem proteção)
+router.get("/api/status", (req, res) => {
+  res.status(200).json({ status: "API is running!" });
+});
+router.post("/api/user/auth", UserController.getAccessToken);
+
+// Aplicar o middleware CheckToken a partir daqui
+router.use(CheckToken);
+
+// Rotas protegidas
+router.get("/api/user/check-token", (req, res) => {
+  res.status(200).json({ message: "Token is valid!" });
+});
 router.get("/api/user/email/:email", UserController.findByEmail);
 
 export default router;
