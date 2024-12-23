fa94f2dcd83c5e1e9651b58d252e4b2c44cdf89e
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-22 23:18:30 -0300

    fix: correct UserException to extend Error constructor

--- auth-api/modules/user/userException/UserException.js
+++ auth-api/modules/user/userException/UserException.js
@@ -1,5 +1,6 @@
 class UserException extends Error {
     constructor(status, message) {
+        super(message); // Chama o construtor da classe pai (Error)
         this.status = status;
         this.message = message;
         this.name = this.constructor.name;
