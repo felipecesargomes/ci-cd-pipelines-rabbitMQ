f1325818c34427725ded0d50bdf297d512953027
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-22 23:16:17 -0300

    chore: fix import path for DbConfig in User model

--- auth-api/modules/user/model/User.js
+++ auth-api/modules/user/model/User.js
@@ -1,5 +1,5 @@
 import Sequelize from "sequelize";
-import sequelize from "../../../config/db/dbConfig.js";
+import sequelize from "../../../config/db/DbConfig.js";
 
 const User = sequelize.define("user", {
   id: {
