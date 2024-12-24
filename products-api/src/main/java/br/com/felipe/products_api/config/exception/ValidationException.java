bffb573e05c8e13d43227be39867e5572d969f06
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-24 18:03:16 -0300

    feat: create type exception validate

---
+++ products-api/src/main/java/br/com/felipe/products_api/config/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package br.com.felipe.products_api.config.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
