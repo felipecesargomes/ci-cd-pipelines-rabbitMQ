286c8cd1e4d5e36e2f8a59bfeaa6e88d17ca8a7c
Felipe Cesar <felipe.calmeida@luizalabs.com>
2024-12-24 18:07:15 -0300

    feat: add CategoryRequest DTO with description field

---
+++ products-api/src/main/java/br/com/felipe/products_api/modules/produto/dto/CategoryRequest.java
@@ -0,0 +1,9 @@
+package br.com.felipe.products_api.modules.produto.dto;
+
+public class CategoryRequest {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+}
