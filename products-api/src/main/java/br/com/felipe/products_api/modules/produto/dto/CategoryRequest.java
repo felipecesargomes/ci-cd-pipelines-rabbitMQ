fcf5b201b011174ee3fab7770ef646ccdda606b5
Felipe <www.felipecesargomes@gmail.com>
2024-12-28 22:36:38 -0300

    refactor: remove unused CategoryRequest class

--- products-api/src/main/java/br/com/felipe/products_api/modules/produto/dto/CategoryRequest.java
+++
@@ -1,9 +0,0 @@
-package br.com.felipe.products_api.modules.produto.dto;
-
-public class CategoryRequest {
-    private String description;
-
-    public String getDescription() {
-        return description;
-    }
-}
