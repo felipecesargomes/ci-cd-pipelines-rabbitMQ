8f63b899666a2c447d59bf617872d7a75a3810d3
Felipe <www.felipecesargomes@gmail.com>
2024-12-28 22:41:12 -0300

    refactor: alter ProductRepository with query methods
    - Added methods to improve querying capabilities:
      - `findByNameContainingIgnoreCase(String name)`.
      - `findByCategoryId(Integer categoryId)`.
      - `findBySupplierId(Integer supplierId)`.
      - `existsBySupplierId(Integer supplierId)`.
      - `existsByCategoryId(Integer categoryId)`.
    - Removed unnecessary `@Repository` annotation as Spring Data handles it automatically.
    
    This refactor enhances query functionality while maintaining repository consistency.

--- products-api/src/main/java/br/com/felipe/products_api/modules/produto/repository/ProductRepository.java
+++ products-api/src/main/java/br/com/felipe/products_api/modules/produto/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package br.com.felipe.products_api.modules.produto.repository;
-import org.springframework.stereotype.Repository;
+
 import br.com.felipe.products_api.modules.produto.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    
+    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByCategoryId(Integer categoryId);
+    List<Product> findBySupplierId(Integer supplierId);
+    boolean existsBySupplierId(Integer supplierId);
+    boolean existsByCategoryId(Integer categoryId);
 }
