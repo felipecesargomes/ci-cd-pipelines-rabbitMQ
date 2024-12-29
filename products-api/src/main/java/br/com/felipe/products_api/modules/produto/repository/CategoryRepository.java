785289793f1d2d38d4103c31672562b29c9e1dc6
Felipe <www.felipecesargomes@gmail.com>
2024-12-28 22:39:44 -0300

    refactor: add method for search categories for description

--- products-api/src/main/java/br/com/felipe/products_api/modules/produto/repository/CategoryRepository.java
+++ products-api/src/main/java/br/com/felipe/products_api/modules/produto/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package br.com.felipe.products_api.modules.produto.repository;
-import org.springframework.stereotype.Repository;
+
 import br.com.felipe.products_api.modules.produto.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    
+    List<Category> findByDescriptionContainingIgnoreCase(String description);
 }
