4ed45b6228dcb9cd69c74be14abef75e77bcac99
Felipe <www.felipecesargomes@gmail.com>
2024-12-28 22:34:19 -0300

    refactor(api): enhance CategoryController with additional endpoints and DTOs

--- products-api/src/main/java/br/com/felipe/products_api/modules/produto/controller/CategoryController.java
+++ products-api/src/main/java/br/com/felipe/products_api/modules/produto/controller/CategoryController.java
@@ -1,27 +1,48 @@
 package br.com.felipe.products_api.modules.produto.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.felipe.products_api.modules.produto.dto.CategoryRequest;
-import br.com.felipe.products_api.modules.produto.dto.CategoryResponse;
+import br.com.felipe.products_api.modules.produto.dto.CategoryDTO;
 import br.com.felipe.products_api.modules.produto.service.CategoryService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/category")
+@RequestMapping("/api/categories")
 public class CategoryController {
-    
+
     @Autowired
     private CategoryService categoryService;
 
+    // Criar uma categoria
     @PostMapping
-    public CategoryResponse save(@RequestBody CategoryRequest request) {
-        return categoryService.save(request);
+    public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO dto) {
+        return ResponseEntity.ok(categoryService.save(dto));
     }
 
-    
+    // Atualizar uma categoria
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Integer id, @RequestBody CategoryDTO dto) {
+        return ResponseEntity.ok(categoryService.update(id, dto));
+    }
+
+    // Excluir uma categoria
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Listar todas as categorias
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    // Buscar categorias por descrição
+    @GetMapping("/search")
+    public ResponseEntity<List<CategoryDTO>> findByDescription(@RequestParam String description) {
+        return ResponseEntity.ok(categoryService.findByDescription(description));
+    }
 }
